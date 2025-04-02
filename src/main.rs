7698abf943ebd4f629a867bbabdaacf509d75a66
Simon Harvey <simon.k.harv@gmail.com>
2025-04-03 07:47:07 +1100

    removed loops in loops

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,28 @@
 #![no_main]
 
 use core::cell::RefCell;
-use core::{u8, usize};
 
 use crate::{hal::I2C, pac::I2C0};
 use cortex_m::{delay::Delay, prelude::_embedded_hal_timer_CountDown};
 use cst816s::TouchGesture;
-use display_interface_spi::SPIInterface;
-use embedded_graphics::image::{Image, ImageRaw, ImageRawBE, ImageRawLE};
 #[allow(clippy::wildcard_imports)]
-use embedded_graphics::mono_font::iso_8859_1::FONT_10X20;
-use embedded_graphics::mono_font::MonoTextStyle;
-use embedded_graphics::text::Text;
 use embedded_hal::delay::DelayNs;
-use embedded_hal::digital::OutputPin;
 use embedded_hal::pwm::SetDutyCycle;
-use gc9a01a_driver::{FrameBuffer, Orientation};
+use gc9a01a_driver::Orientation;
 use mcp230xx::{Level, Mcp23017, Mcp230xx};
 use panic_halt as _;
 use rp_pico as bsp;
 use rp_pico::hal::gpio::bank0::{Gpio17, Gpio28};
 use rp_pico::hal::gpio::{FunctionI2C, FunctionPwm, Pin};
-use tinybmp::{Bmp, RawBmp};
-use tinytga::Tga;
+use rp_pico::hal::multicore::Stack;
 use usb_device::{class_prelude::*, prelude::*};
 use usbd_human_interface_device::prelude::*;
 
 use bsp::entry;
 use fugit::{ExtU32, RateExtU32};
 
+use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::*;
-use embedded_graphics::{
-    pixelcolor::Rgb565,
-    primitives::{Circle, PrimitiveStyleBuilder, Rectangle, Triangle},
-};
 
 use bsp::hal::{
     self,
@@ -58,7 +47,18 @@ use reports::{twin_stick_layout, twin_stick_layout::get_twin_stick_report};
 
 const SCREEN_WIDTH: u32 = 240;
 const SCREEN_HEIGHT: u32 = 240;
-const BUFFER_SIZE: usize = (SCREEN_WIDTH * SCREEN_HEIGHT * 2) as usize;
+
+#[derive(PartialEq, Eq, Default)]
+#[repr(u8)]
+enum Mode {
+    #[default]
+    None,
+    AllButton,
+    SSBM,
+    StreetFighter,
+}
+
+static mut CORE1_STACK: Stack<4096> = Stack::new();
 
 struct DelayWrapper<'a> {
     delay: &'a mut Delay,
@@ -99,7 +99,6 @@ fn main() -> ! {
     .unwrap();
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
     let timer = bsp::hal::timer::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
 
     let pins = bsp::Pins::new(
@@ -109,7 +108,7 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // USB
+    // USB turn on the usb bus
     let usb_bus = UsbBusAllocator::new(UsbBus::new(
         pac.USBCTRL_REGS,
         pac.USBCTRL_DPRAM,
@@ -123,13 +122,10 @@ fn main() -> ! {
     let spi_mosi = pins.gpio11.into_function::<gpio::FunctionSpi>();
     let spi_miso = pins.gpio12.into_function::<gpio::FunctionSpi>();
     let spi_cs = pins.gpio9.into_push_pull_output();
-
     let valid_pinout = (spi_mosi, spi_miso, spi_sclk);
 
     // Create an SPI driver instance for the SPI1 device
     let spi = spi::Spi::<_, _, _, 8>::new(pac.SPI1, valid_pinout);
-
-    // Exchange the uninitialised SPI driver for an initialised one
     let spi = spi.init(
         &mut pac.RESETS,
         clocks.peripheral_clock.freq(),
@@ -140,15 +136,13 @@ fn main() -> ! {
     let dc_pin = pins.gpio8.into_push_pull_output();
     let rst_pin = pins.gpio13.into_push_pull_output();
 
-    // initialize PWM for backlight
+    // initialize PWM for backlight on channel_b
     let pwm_slices = pwm::Slices::new(pac.PWM, &mut pac.RESETS);
 
-    // Configure PWM6
     let mut pwm = pwm_slices.pwm4;
     pwm.set_ph_correct();
     pwm.enable();
 
-    // Output channel B on PWM4 to GPIO 25
     let mut channel = pwm.channel_b;
     let led_pin = pins.led.into_function::<FunctionPwm>();
 
@@ -188,6 +182,7 @@ fn main() -> ! {
 
     let i2c_ref_cell = RefCell::new(i2c0_pins);
 
+    // Setup MCP1
     let mut mcp1 = Mcp230xx::<
         I2C<
             I2C0,
@@ -200,6 +195,7 @@ fn main() -> ! {
     >::new(&i2c_ref_cell, 0x27)
     .unwrap();
 
+    // Setup MCP2
     let mut mcp2 = Mcp230xx::<
         I2C<
             I2C0,
@@ -212,7 +208,7 @@ fn main() -> ! {
     >::new(&i2c_ref_cell, 0x26)
     .unwrap();
 
-    // Initialize all the buttons
+    // Initialize all the buttons for MCP1
     inputs::init_button(&mut mcp1, Mcp23017::A0);
     inputs::init_button(&mut mcp1, Mcp23017::A1);
     inputs::init_button(&mut mcp1, Mcp23017::A2);
@@ -231,6 +227,7 @@ fn main() -> ! {
     inputs::init_button(&mut mcp1, Mcp23017::B6);
     inputs::init_button(&mut mcp1, Mcp23017::B7);
 
+    // Initialize all the buttons for MCP2
     inputs::init_button(&mut mcp2, Mcp23017::A0);
     inputs::init_button(&mut mcp2, Mcp23017::A1);
     inputs::init_button(&mut mcp2, Mcp23017::A2);
@@ -250,7 +247,7 @@ fn main() -> ! {
     inputs::init_button(&mut mcp2, Mcp23017::B7);
 
     // Create display driver
-    let mut clear_colour = Rgb565::CSS_BLACK;
+    let clear_colour = Rgb565::CSS_BLACK;
 
     let mut delay_wrapper = DelayWrapper::new(&mut delay);
 
@@ -263,7 +260,7 @@ fn main() -> ! {
         SCREEN_WIDTH,
         SCREEN_HEIGHT,
     );
-    //display.hard_reset(&mut delay_wrapper).unwrap();
+
     display.init(&mut delay_wrapper).unwrap();
     display
         .set_orientation(&Orientation::PortraitSwapped)
@@ -272,39 +269,57 @@ fn main() -> ! {
 
     channel.set_duty_cycle_percent(50).unwrap();
 
-    //let mut buffer: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
-    //let mut framebuffer = FrameBuffer::new(&mut buffer, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    //let spi_interface = SPIInterface::new(spi, dc_pin, spi_cs);
-
-    //let mut display = gc9a01a::GC9A01A::new(spi_interface, rst_pin, channel);
-    //display.reset(&mut delay).unwrap();
-    //display.initialize(&mut delay).unwrap();
-    //display.clear(clear_colour).unwrap();
-    //display.set_backlight(10000);
-
-    // Draw Stuff
-    let text_style = MonoTextStyle::new(&FONT_10X20, Rgb565::CSS_LIGHT_GOLDENROD_YELLOW);
-    let style = PrimitiveStyleBuilder::new()
-        .stroke_width(5)
-        .stroke_color(Rgb565::CSS_ORCHID)
-        .build();
-
-    // Input Polling rate
+    // Set the input Polling rate
     let mut input_count_down = timer.count_down();
     input_count_down.start(1.millis());
+
     let mut cleared = false;
 
-    loop {
+    // Touchpad Gestures for future use (using a different core or something)
+    //if let Some(evt) = touchpad.read_one_touch_event(true) {
+    //    match evt.gesture {
+    //        TouchGesture::LongPress => {
+    //            //display.set_backlight(55000);
+    //        }
+    //        TouchGesture::SingleClick => {
+    //            usb_dev.force_reset().unwrap();
+    //            break;
+    //        }
+    //        _ => (),
+    //    };
+    //}
+
+    let mut selected_mode = Mode::None;
+
+    while selected_mode == Mode::None {
         if !cleared {
             let bmp_data = include_bytes!("../assets/menu.raw");
             display.draw_image(bmp_data).unwrap();
             cleared = true;
         }
 
+        // All Buttons
         if mcp1.gpio(Mcp23017::A5).unwrap() == Level::High {
-            //display.clear_screen(clear_colour.into_storage()).unwrap();
+            selected_mode = Mode::AllButton;
+            break;
+        }
+        // SSBM
+        if mcp1.gpio(Mcp23017::A4).unwrap() == Level::High {
+            selected_mode = Mode::SSBM;
+            break;
+        }
+        // StreetFighter
+        if mcp1.gpio(Mcp23017::B2).unwrap() == Level::High {
+            selected_mode = Mode::StreetFighter;
+            break;
+        }
+    }
 
+    match selected_mode {
+        Mode::None => {
+            exit();
+        }
+        Mode::AllButton => {
             let mut all_button_joy = UsbHidClassBuilder::new()
                 .add_device(all_button_layout::AllButtonConfig::default())
                 .build(&usb_bus);
@@ -322,20 +337,6 @@ fn main() -> ! {
 
             // Begin Loop
             loop {
-                // Touchpad Gestures
-                if let Some(evt) = touchpad.read_one_touch_event(true) {
-                    match evt.gesture {
-                        TouchGesture::LongPress => {
-                            //display.set_backlight(55000);
-                        }
-                        TouchGesture::SingleClick => {
-                            usb_dev.force_reset().unwrap();
-                            break;
-                        }
-                        _ => (),
-                    };
-                }
-
                 if input_count_down.wait().is_ok() {
                     let mut bank_a1 = allbtn::read_bank_a(&mut mcp1);
                     let mut bank_b1 = allbtn::read_bank_b(&mut mcp1);
@@ -359,7 +360,7 @@ fn main() -> ! {
                 if usb_dev.poll(&mut [&mut all_button_joy]) {}
             }
         }
-        if mcp1.gpio(Mcp23017::A4).unwrap() == Level::High {
+        Mode::SSBM => {
             let bmp_data = include_bytes!("../assets/ssbm.raw");
             display.draw_image(bmp_data).unwrap();
 
@@ -367,7 +368,7 @@ fn main() -> ! {
                 .add_device(twin_stick_layout::TwinStickConfig::default())
                 .build(&usb_bus);
 
-            let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x1209, 0x0001))
+            let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x1209, 0x0002))
                 .strings(&[StringDescriptors::default()
                     .manufacturer("skharv")
                     .product("touchbox(ssbmbox)")
@@ -377,21 +378,6 @@ fn main() -> ! {
 
             // Begin Loop
             loop {
-                // Touchpad Gestures
-                if let Some(evt) = touchpad.read_one_touch_event(true) {
-                    match evt.gesture {
-                        TouchGesture::LongPress => {
-                            //display.set_backlight(55000);
-                        }
-                        TouchGesture::SingleClick => {
-                            usb_dev.force_reset().unwrap();
-                            break;
-                        }
-
-                        _ => (),
-                    };
-                }
-
                 if input_count_down.wait().is_ok() {
                     let mut bank_a1 = allbtn::read_bank_a(&mut mcp1);
                     let mut bank_b1 = allbtn::read_bank_b(&mut mcp1);
@@ -415,7 +401,7 @@ fn main() -> ! {
                 if usb_dev.poll(&mut [&mut twin_stick_joy]) {}
             }
         }
-        if mcp1.gpio(Mcp23017::B2).unwrap() == Level::High {
+        Mode::StreetFighter => {
             let bmp_data = include_bytes!("../assets/sf.raw");
             display.draw_image(bmp_data).unwrap();
 
@@ -423,7 +409,7 @@ fn main() -> ! {
                 .add_device(classic_layout::ClassicConfig::default())
                 .build(&usb_bus);
 
-            let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x1209, 0x0001))
+            let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x1209, 0x0003))
                 .strings(&[StringDescriptors::default()
                     .manufacturer("skharv")
                     .product("touchbox(fightbox)")
@@ -433,20 +419,6 @@ fn main() -> ! {
 
             // Begin Loop
             loop {
-                // Touchpad Gestures
-                if let Some(evt) = touchpad.read_one_touch_event(true) {
-                    match evt.gesture {
-                        TouchGesture::LongPress => {
-                            //display.set_backlight(55000);
-                        }
-                        TouchGesture::SingleClick => {
-                            break;
-                        }
-
-                        _ => (),
-                    };
-                }
-
                 if input_count_down.wait().is_ok() {
                     let mut bank_a1 = allbtn::read_bank_a(&mut mcp1);
                     let mut bank_b1 = allbtn::read_bank_b(&mut mcp1);
@@ -472,6 +444,8 @@ fn main() -> ! {
         }
     }
 }
+
+pub fn control_touch() {}
 
 pub fn exit() -> ! {
     loop {
